6cfe46b1bb7531f53690f4286003a0cd99657812
Michael J Coss <mjcoss@gmail.com>
2017-01-23 10:11:35 -0500

    Initial commit for robot code
    Signed-off-by: Michael J Coss <mjcoss@gmail.com>

---
+++ src/Brahe.h
@@ -0,0 +1,17 @@
+/*
+ * Brahe.h
+ *
+ *  Created on: Jan 20, 2017
+ *      Author: Michael
+ */
+
+#ifndef SRC_BRAHE_H_
+#define SRC_BRAHE_H_
+
+enum DriveMotors { LEFT_FRONT_DRIVEMOTOR, RIGHT_FRONT_DRIVEMOTOR, LEFT_REAR_DRIVEMOTOR, RIGHT_REAR_DRIVEMOTOR, NUM_DRIVE_MOTORS };
+enum Joysticks { LEFT_JOYSTICK, RIGHT_JOYSTICK, NUM_JOYSTICKS };
+
+
+
+
+#endif /* SRC_BRAHE_H_ */
---
+++ src/Robot.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+#include <WPILib.h>
+#include <CANTalon.h>
+#include <Brahe.h>
+
+class Robot: public frc::IterativeRobot
+{
+public:
+	CANTalon *leftMotor, *rightMotor;
+	Joystick *leftJoystick, *rightJoystick;
+
+	void
+	RobotInit()
+	{
+		leftMotor  = new CANTalon(LEFT_FRONT_DRIVEMOTOR);
+		rightMotor = new CANTalon(RIGHT_FRONT_DRIVEMOTOR);
+
+		leftJoystick  = new Joystick(LEFT_JOYSTICK);
+		rightJoystick = new Joystick(RIGHT_JOYSTICK);
+
+		chooser.AddDefault(autoNameDefault, autoNameDefault);
+		chooser.AddObject(autoNameCustom, autoNameCustom);
+		frc::SmartDashboard::PutData("Auto Modes", &chooser);
+	}
+
+	void
+	AutonomousInit() override
+	{
+		autoSelected = chooser.GetSelected();
+		// std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
+		std::cout << "Auto selected: " << autoSelected << std::endl;
+
+		if (autoSelected == autoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void
+	AutonomousPeriodic()
+	{
+		if (autoSelected == autoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+		double y1, y2;
+
+		y1 = leftJoystick->GetY();
+		y2 = rightJoystick->GetY();
+
+		leftMotor->Set(y1);
+		rightMotor->Set(y2);
+	}
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
+
+private:
+	frc::LiveWindow* lw = LiveWindow::GetInstance();
+	frc::SendableChooser<std::string> chooser;
+	const std::string autoNameDefault = "Default";
+	const std::string autoNameCustom = "My Auto";
+	std::string autoSelected;
+};
+
+START_ROBOT_CLASS(Robot)
