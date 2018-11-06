eba83a18f318c2f1d02e8dcd1bb33fab0c751c24
joanlarmore <joanlarmore@gmail.com>
2018-11-05 19:34:29 -0500

    Turns light on and tank drive

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
 #include <math.h>
-
 #include <IterativeRobot.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
-
 #include <WPILib.h>
 #include <ctre/Phoenix.h>
 #include <Brahe.h>
-
 
 class Robot: public frc::IterativeRobot
 {
@@ -20,16 +16,26 @@ public:
 	WPI_TalonSRX *climbMotor;
 	Joystick *leftJoystick, *rightJoystick;
 	Relay *lightswitch;
-
+	frc::DifferentialDrive *drive;
 	void
 	RobotInit()
 	{
+		lightswitch = new Relay(LIGHT_SWITCH);
+		leftMotor = new WPI_TalonSRX(LEFT_DRIVEMOTOR);
+		leftSlave = new WPI_TalonSRX(LEFT_SLAVEMOTOR);
+		rightMotor = new WPI_TalonSRX(RIGHT_DRIVEMOTOR);
+		rightSlave = new WPI_TalonSRX(RIGHT_SLAVEMOTOR);
+		leftJoystick = new Joystick(LEFT_JOYSTICK);
+		rightJoystick = new Joystick(RIGHT_JOYSTICK);
+		drive = new DifferentialDrive(*leftMotor, *rightMotor);
+		leftSlave->Set(ControlMode::Follower, leftMotor->GetDeviceID());
+		rightSlave->Set(ControlMode::Follower, rightMotor->GetDeviceID());
 
 	}
-
 	void
 	RobotPeriodic()
 	{
+
 	}
 
 	void
@@ -74,19 +80,20 @@ public:
 	void
 	TeleopInit()
 	{
-
+		lightswitch->Set(Relay::kForward);
 	}
 
 	void
 	TeleopPeriodic()
 	{
-		
+		drive->TankDrive(leftJoystick->GetY(), rightJoystick->GetY(), true);
 	}
 
 	void
 	TestInit()
 	{
-    }
+
+	}
 
 	void
 	TestPeriodic()
