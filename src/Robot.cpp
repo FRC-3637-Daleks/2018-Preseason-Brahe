3c59d53d4f4bc7b35677006ae7866df5a5ee45a7
joanlarmore <joanlarmore@gmail.com>
2018-11-05 20:16:36 -0500

    LED light works

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -21,6 +21,25 @@ public:
 	RobotInit()
 	{
 		lightswitch = new Relay(LIGHT_SWITCH);
+		leftMotor = new WPI_TalonSRX(LEFT_DRIVEMOTOR);
+		leftSlaveMotor = new WPI_TalonSRX(LEFT_SLAVEMOTOR);
+		rightMotor = new WPI_TalonSRX(RIGHT_DRIVEMOTOR);
+		rightSlaveMotor = new WPI_TalonSRX(RIGHT_SLAVEMOTOR);
+		leftJoystick = new Joystick(LEFT_JOYSTICK);
+		rightJoystick = new Joystick(RIGHT_JOYSTICK);
+
+		drive = new DifferentialDrive(*leftMotor, *rightMotor);
+		leftSlaveMotor->Set(ControlMode::Follower, leftMotor->GetDeviceID());
+		rightSlaveMotor->Set(ControlMode::Follower, rightMotor->GetDeviceID());
+		leftMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0,
+				10);
+		rightMotor->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0,
+				10);
+		leftMotor->SetSelectedSensorPosition(0, 0, 10);
+		rightMotor->SetSelectedSensorPosition(0, 0, 10);
+		leftMotor->GetSensorCollection().SetQuadraturePosition(0, 0);
+		rightMotor->GetSensorCollection().SetQuadraturePosition(0, 0);
+
 
 	}
 	void
@@ -77,6 +96,21 @@ public:
 	void
 	TeleopPeriodic()
 	{
+		drive->TankDrive(leftJoystick->GetY(), rightJoystick->GetY(), true);
+
+				double lspeed =
+						leftMotor->GetSensorCollection().GetQuadratureVelocity();
+				double rspeed =
+						rightMotor->GetSensorCollection().GetQuadratureVelocity();
+				frc::SmartDashboard::PutNumber("Left Poop Speed", lspeed);
+				frc::SmartDashboard::PutNumber("Right Motor Speed", rspeed);
+
+				if (lspeed == 0 and rspeed == 0) {
+					if (leftJoystick->GetTrigger())
+						gearShift->Set(true);
+					if (rightJoystick->GetTrigger())
+						gearShift->Set(false);
+				}
 	}
 
 	void
